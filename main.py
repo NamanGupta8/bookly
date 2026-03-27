cc3c7b3916785b845fe12bfa2892d294ecb0b302
Naman Gupta <guptanaman008@gmail.com>
2026-03-27 18:15:10 +0530

    Get request
    Basic get request, path parameters and query parameters

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 @app.get("/")
 async def read_root() -> dict[str, str]:
     return {"message": "Server working"}
 
-@app.get("/greet/{name}")
-async def greet_name(name:str)-> dict[str, str]:
-    return {"message": f"Hello {name}"}
+# Any parameter in function, will be considered a query parameter if not defined in path. Defined in path is path parameter
+@app.get("/greet")
+async def greet_name(name: Optional[str] = "User", age: int = 0)-> dict[str, str]:
+    return {"message": f"Hello {name} !! Age {age}"}
+
