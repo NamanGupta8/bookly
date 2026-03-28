f6d2c864e00fca024d78ff8de2fa958b0a1f03be
Naman Gupta <guptanaman008@gmail.com>
2026-03-28 13:41:40 +0530

    request headers

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from typing import Optional
-
+from pydantic import BaseModel
 app = FastAPI()
+
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+
+
 @app.get("/")
 async def read_root() -> dict[str, str]:
     return {"message": "Server working"}
@@ -11,3 +17,19 @@ async def read_root() -> dict[str, str]:
 async def greet_name(name: Optional[str] = "User", age: int = 0)-> dict[str, str]:
     return {"message": f"Hello {name} !! Age {age}"}
 
+@app.post('/create_book')
+async def create_book(book_data: BookCreateModel)-> dict[str, str]:
+    return {
+        "title": book_data.title,
+        "author": book_data.author
+        }
+
+@app.get('/get_headers')
+async def get_headers(
+    accept:str = Header(None),
+    content_type:str = Header(None)
+):
+    request_headers = {}
+    request_headers["Accept"] = accept
+    request_headers["Content-Type"] = content_type
+    return request_headers
